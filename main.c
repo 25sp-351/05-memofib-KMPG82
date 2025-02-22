7d6549f79522ba6dd1de1565d53b11ce5f019fa2
KMPG82 <kevinponting82@gmail.com>
2025-02-22 13:44:35 -0800

    moved stuff around

--- main.c
+++ main.c
@@ -6,40 +6,13 @@
 
 typedef long (*long_func_ptr)(int param);
 
+long fibonacci(int of_num);
+long cache_func(int of_num);
+long_func_ptr init_cache(long_func_ptr real_provider);
+
 long_func_ptr fibonacci_provider;
 long_func_ptr _original_provider;
 long _memoization_data[ARRAY_SIZE];
-
-/* calculates fibonacci for a number */
-long fibonacci(int of_num) {
-    if (of_num <= 1)
-        return of_num;
-
-    return (*fibonacci_provider)(of_num - 1) +
-           (*fibonacci_provider)(of_num - 2);
-}
-
-/* checks cache if the value has been stored */
-long cache_func(int of_num) {
-    if (of_num > MAX_MEMOIZED)
-        return (*_original_provider)(of_num);
-
-    if (_memoization_data[of_num] == NO_VALUE_YET) {
-        _memoization_data[of_num] = (*_original_provider)(of_num);
-    }
-    
-    return _memoization_data[of_num];
-}
-
-/* initializes cache */
-long_func_ptr init_cache(long_func_ptr real_provider) {
-    for (int ix = 0; ix < ARRAY_SIZE; ix++)
-        _memoization_data[ix] = NO_VALUE_YET;
-
-    _original_provider = real_provider;
-    
-    return cache_func;
-}
 
 int main() {
     fibonacci_provider = init_cache(fibonacci);
@@ -62,4 +35,34 @@ int main() {
     printf("%lu\n", (*fibonacci_provider)(90));
 
     return 0;
+}
+
+/* calculates fibonacci for a number */
+long fibonacci(int of_num) {
+    if (of_num <= 1)
+        return of_num;
+
+    return (*fibonacci_provider)(of_num - 1) +
+           (*fibonacci_provider)(of_num - 2);
+}
+
+/* checks cache if the value has been stored */
+long cache_func(int of_num) {
+    if (of_num > MAX_MEMOIZED)
+        return (*_original_provider)(of_num);
+
+    if (_memoization_data[of_num] == NO_VALUE_YET)
+        _memoization_data[of_num] = (*_original_provider)(of_num);
+
+    return _memoization_data[of_num];
+}
+
+/* initializes cache */
+long_func_ptr init_cache(long_func_ptr real_provider) {
+    for (int ix = 0; ix < ARRAY_SIZE; ix++)
+        _memoization_data[ix] = NO_VALUE_YET;
+
+    _original_provider = real_provider;
+
+    return cache_func;
 }
